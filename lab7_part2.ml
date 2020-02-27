b662ad115d93227125cf05b6a7ed55e2f0ee6abd
Karen Chen <kchen6600@gmail.com>
2020-02-26 22:12:29 -0500

    complete

--- lab7_part2.ml
+++ lab7_part2.ml
@@ -4,40 +4,40 @@
  *)
 
 (* Objective: This lab practices concepts of modules, including files
-as modules, signatures, and polymorphic abstract data types.
+   as modules, signatures, and polymorphic abstract data types.
 
-There are 4 total parts to this lab. Please refer to the following
-files to complete all exercises:
+   There are 4 total parts to this lab. Please refer to the following
+   files to complete all exercises:
 
    lab7_part1.ml -- Part 1: Implementing modules
--> lab7_part2.ml -- Part 2: Files as modules (this file)
+   -> lab7_part2.ml -- Part 2: Files as modules (this file)
    lab7_part3.ml -- Part 3: Interfaces as abstraction barriers
    lab7_part4.ml -- Part 4: Polymorphic abstract types
- *)
+*)
 
 (*======================================================================
-Part 2: Files as modules
+  Part 2: Files as modules
 
-A useful feature of OCaml is that it *automatically* wraps functions
-and values that are defined in a single file into a module named after
-that file. The module name is the name of the file with the first
-letter capitalized. This functionality is in addition to the manual
-definition of modules as you've just used in Part 1, but it is a
-convenient way of separating code into separate namespaces when
-writing a large program.
+  A useful feature of OCaml is that it *automatically* wraps functions
+  and values that are defined in a single file into a module named after
+  that file. The module name is the name of the file with the first
+  letter capitalized. This functionality is in addition to the manual
+  definition of modules as you've just used in Part 1, but it is a
+  convenient way of separating code into separate namespaces when
+  writing a large program.
 
-There are other source files included in this lab, other than the
-lab7_partn.ml files. The file color.ml contains an implementation of a
-system for managing colors. (Recall the idea of colors as consisting
-of values for three color channels (red, green, and blue) from lab 5.)
-Take a look at it to see what functions and values it contains,
-including a type for colors, and a means for converting values for the
-three color channels into the abstract color type, extracting the
-channels individually from colors, and converting some standard color
-names to this color representation.
+  There are other source files included in this lab, other than the
+  lab7_partn.ml files. The file color.ml contains an implementation of a
+  system for managing colors. (Recall the idea of colors as consisting
+  of values for three color channels (red, green, and blue) from lab 5.)
+  Take a look at it to see what functions and values it contains,
+  including a type for colors, and a means for converting values for the
+  three color channels into the abstract color type, extracting the
+  channels individually from colors, and converting some standard color
+  names to this color representation.
 
-With the exception of Exercise 2A, you will need to modify **only**
-color.ml to complete the exercises below.
+  With the exception of Exercise 2A, you will need to modify **only**
+  color.ml to complete the exercises below.
 
     A digression on accessing other modules:
 
@@ -77,44 +77,46 @@ color.ml to complete the exercises below.
     in the corresponding .mli files. For that, you'll have to use the
     compiler (with ocamlbuild for instance).
 
-........................................................................
-Exercise 2A: Replace the `0` in the expression below with an
-expression that extracts the red channel of the color named Red,
-thereby naming the result `red_channel`.
-......................................................................*)
+  ........................................................................
+  Exercise 2A: Replace the `0` in the expression below with an
+  expression that extracts the red channel of the color named Red,
+  thereby naming the result `red_channel`.
+  ......................................................................*)
+open Color;;
 
-let red_channel : int = 0 ;;
+
+let red_channel : int = (red (color_named Red)) ;;
 
 (* Let's investigate one way that a signature can be useful. Although
-color.ml contains an implementation of a basic color module, the
-implementation is unintuitive and obscure -- truly horrid in fact. (We
-did that on purpose.) You will want to change the implementation of
-color.ml, rewriting it wholesale. At the same time, you'll want to
-guarantee to users of the module (like this file itself!) that the
-functionality stays the same; the way to do this is through module
-signatures.
+   color.ml contains an implementation of a basic color module, the
+   implementation is unintuitive and obscure -- truly horrid in fact. (We
+   did that on purpose.) You will want to change the implementation of
+   color.ml, rewriting it wholesale. At the same time, you'll want to
+   guarantee to users of the module (like this file itself!) that the
+   functionality stays the same; the way to do this is through module
+   signatures.
 
-......................................................................
-Exercise 2B: Add a file color.mli, in which you define an appropriate
-signature for the Color module. Consider which types and values you
-want revealed to the user and which you would prefer to be hidden.
+   ......................................................................
+   Exercise 2B: Add a file color.mli, in which you define an appropriate
+   signature for the Color module. Consider which types and values you
+   want revealed to the user and which you would prefer to be hidden.
 
-Once you have color.mli implemented, you should still be able to
-compile color.ml and run color.byte.
-......................................................................*)
+   Once you have color.mli implemented, you should still be able to
+   compile color.ml and run color.byte.
+   ......................................................................*)
 
 
 (*......................................................................
-Exercise 2C:
+  Exercise 2C:
 
-In the file color.ml, modify the implementation of a color module as
-you see fit. Make the design choices that you think would be best for
-a color module implementation. In particular, you should be able to
-come up with a solution that is **much** clearer and more transparent
-than the one currently in color.ml.
+  In the file color.ml, modify the implementation of a color module as
+  you see fit. Make the design choices that you think would be best for
+  a color module implementation. In particular, you should be able to
+  come up with a solution that is **much** clearer and more transparent
+  than the one currently in color.ml.
 
-To pass the unit tests, you'll want the RGB values for the colors in
-the color_name type to have the following values:
+  To pass the unit tests, you'll want the RGB values for the colors in
+  the color_name type to have the following values:
 
      R  |  G  |  B  | Color
     ----|-----|-----|--------
@@ -126,18 +128,18 @@ the color_name type to have the following values:
      75 |   0 | 130 | Indigo
     240 | 130 | 240 | Violet
 
-......................................................................*)
+  ......................................................................*)
 
 
 (* Here's the payoff: A user who uses the color module, by virtue of
-having to stay within the color.mli interface, will not notice **any
-difference at all** between the two implementations in color.ml. The
-underlying implementation can be changed any time in any way, so long
-as the functionality provided stays consistent with the signature.
+   having to stay within the color.mli interface, will not notice **any
+   difference at all** between the two implementations in color.ml. The
+   underlying implementation can be changed any time in any way, so long
+   as the functionality provided stays consistent with the signature.
 
-For instance, consider the List module that you are familiar with by
-now.  You never needed to worry about how the List module was
-implemented in order to use it, you only needed to understand the
-interface.
+   For instance, consider the List module that you are familiar with by
+   now.  You never needed to worry about how the List module was
+   implemented in order to use it, you only needed to understand the
+   interface.
 
-Compartmentalization ftw! *)
+   Compartmentalization ftw! *)
